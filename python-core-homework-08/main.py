9142a35b02aa73f7678eea448be7f4cedf8290c4
Kyrylo <140982410+KyryloChalov@users.noreply.github.com>
2023-10-01 15:39:44 +0300

    Add files via upload

--- python-core-homework-08/main.py
+++ python-core-homework-08/main.py
@@ -1,49 +1,63 @@
+"""
+    Домашне завдання № 8: реалізувати функцію для виведення списку колег, яких потрібно привітати з днем народження на тижні.
+        ### version 1.3.2
+    особливості реалізації:
+    21-22   - якщо список користувачів порожній
+    27      - визначаємо сьогодняшню дату через date.today() 
+    28      - рядок для тестування
+    30-31   - якщо сьогодні понеділок - треба вивести дн за минулі вихідні, а наступні вихідні - вимкнути
+    33-34   - якщо сьогодні неділя - треба врахувати вчорашні дн (за суботу)
+    40      - переносимо дату дн на поточний рік
+    42-43   - якщо дн вже минув - перенести його на наступний рік (на 365 days)
+    45-49   - вираховуємо день тижня для дн юзера, якщо до нього не більше тижня. формуємо списки юзерів з дн по днях тижня
+    46-47   - якщо дн випадає на вихідні - переносимо їх на наступний понеділок
+    51-54   - збираємо словник з днів тижня та списків юзерів, що мають дн у конкретний день тижня
+    53      - порожні дні тижня пропускаємо
+"""
 from datetime import date, datetime, timedelta
-
-
+    
 def get_birthdays_per_week(users):
 
     if users == []:
         users = {}
 
-        
-    result = {}
-    
     WEEKDAYS = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-    list_for_weekday = [[], [], [], [], [], [], []]
+    list_for_weekdays = [[], [], [], [], [], [], []]
     
     d_today = date.today() 
-    # d_today = date(year=2023, month=10, day=2) # для отладки
-    d_end = d_today + timedelta(days=7)
+    # d_today = date(year=2023, month=10, day=3) # рядок для тестування!
     
-    for user in users:
-        # якщо дн вже минув - перенести його на наступний рік
-        user["birthday"] = datetime(year=d_today.year, month=user["birthday"].month, day=user["birthday"].day)
-        if user["birthday"].date() < d_today:
-            user["birthday"] = user["birthday"].replace(year=d_today.year+1)
+    if d_today.weekday() == 0: 
+        d_today = d_today + timedelta(-2)
+    
+    if d_today.weekday() == 6: 
+        d_today = d_today + timedelta(-1)
+    
+    d_end = d_today + timedelta(7)
         
-        # вираховуємо день тижня для дн юзера, якщо до нього не більше тижня
+    for user in users:
+        
+        user["birthday"] = datetime(year=d_today.year, month=user["birthday"].month, day=user["birthday"].day)
+        
+        if user["birthday"].date() < d_today:
+            user["birthday"] = user["birthday"] + timedelta(365)
+         
         if user["birthday"].date() < (d_end):
-            
-            # якщо сьогодні - не понеділок, переносимо вихідні на понеділок (наступного тижня)
-            # якщо сьогодні понеділок, дн що припали на вихідні виводятся у вихідні 
-            # (може прибрати их зовсім??? бо на сьогодняшній понеділок переносити не можна)
-            if d_today.weekday() != 0 and user["birthday"].weekday() in [5, 6]: 
-                list_for_weekday[0].append(user["name"])
+            if user["birthday"].weekday() in [5, 6]: 
+                list_for_weekdays[0].append(user["name"])
             else:
-                # запісати ім'я у відповідний список, з яких потім зробити словник
-                list_for_weekday[user["birthday"].weekday()].append(user["name"])
-                                
-    # збираємо словник з днів тижня та списків юзерів, що мають дн у конкретний день тижня
+                list_for_weekdays[user["birthday"].weekday()].append(user["name"])
+                
+    users = {}
     for i in range(len(WEEKDAYS)):
-        if list_for_weekday[i] != []:     # порожні дні тижня пропускаємо
-            result.update({WEEKDAYS[i] : list_for_weekday[i]})
+        if list_for_weekdays[i] != []: 
+            users.update({WEEKDAYS[i] : list_for_weekdays[i]})
     
-    return result 
+    return users 
 
 
 if __name__ == "__main__":
-    users = [
+    users = [               # дані для тестування
         {"name": "1. Jan Koum", "birthday": datetime(1976, 1, 1).date()},
         {"name": "2. Bill Gates", "birthday": datetime(1955, 12, 28).date()},
         {"name": "3. John", "birthday": datetime(2000, 10, 2).date()},
@@ -51,8 +65,8 @@ if __name__ == "__main__":
         {"name": "5. Johnfh", "birthday": datetime(1956, 3, 28).date(),},
         {"name": "6. Doedffhg", "birthday": datetime(1963, 10, 8).date(),},
         {"name": "7. Alice", "birthday": datetime(1958, 10, 4).date()},
-        {"name": "8. Johnkjnhg", "birthday": datetime(1985, 9, 22).date(),},
-        {"name": "9. Doesdf", "birthday": datetime(2001, 1, 8).date(),},
+        {"name": "8. Johnkjnhg", "birthday": datetime(1985, 10, 1).date(),},
+        {"name": "9. Doesdf", "birthday": datetime(2001, 10, 6).date(),},
         {"name": "10. Aliceoi", "birthday": datetime(1983, 10, 7).date()}
     ]
 
